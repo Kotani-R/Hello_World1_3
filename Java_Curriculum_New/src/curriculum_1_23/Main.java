48a1f1b242e9a9a7cd9f18f13ff9d662acf30f8c
Kotani <lyise771@gmail.com>
2023-05-16 17:59:26 +0900

    カリキュラム1.23

--- Java_Curriculum_New/src/curriculum_1_23/Main.java
+++ Java_Curriculum_New/src/curriculum_1_23/Main.java
@@ -16,9 +16,12 @@ public class Main {
 		// lionインスタンスを生成
 		Lion lion = new Lion("ライオン", 2.1 , 80);
 		
-		
+		// コンソール出力
+		//「動物名：ライオン」
 		System.out.println("動物名："+ lion.getName());
+		//「体長：2.1m」
 		System.out.println("体長："+ lion.getLength()+"m");
+		//「速度：80km/h」
 		System.out.println("速度："+ lion.getSpeed()+"km/h");
 	}
 
